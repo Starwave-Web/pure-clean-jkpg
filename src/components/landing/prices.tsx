324b9a50193c0be364932e2946966ca7957340f1
udvardiendre <udvardiendre@gmail.com>
2025-04-28 17:49:07 +0200

    price fixes

--- src/components/landing/prices.tsx
+++ src/components/landing/prices.tsx
@@ -1,14 +1,16 @@
-"use client"
+"use client";
 import { SECTIONS } from "@/src/lib/const";
 import { useTranslations } from "next-intl";
 import { useState } from "react";
 import { Button } from "../ui/button";
+import { Ghost } from "lucide-react";
 
 const Prices = () => {
   const t = useTranslations("prices");
-  const [rutMultiplier, setRutMultiplier] = useState(1)
+  const [rutMultiplier, setRutMultiplier] = useState(1);
+  const [toggleInfo, setToggleInfo] = useState(false);
 
-  const innitialPrice = process.env.NEXT_PUBLIC_INITIAL_PRICE as unknown as number
+  // const innitialPrice = process.env.NEXT_PUBLIC_INITIAL_PRICE as unknown as number
 
   return (
     <section id={SECTIONS.PRICES} className="w-full px-4 scroll-mt-28">
@@ -21,7 +23,12 @@ const Prices = () => {
             {t("description")}
           </p>
           <div>
-            <Button className="bg-white text-primary-blue hover:bg-white/90 active:bg-light-gray" onClick={() => setRutMultiplier(rutMultiplier === 1 ? 2 : 1)}>{rutMultiplier === 1 ?  t("applyRut") : t("removeRut")}</Button>
+            <Button
+              className="bg-white text-primary-blue hover:bg-white/90 active:bg-light-gray"
+              onClick={() => setRutMultiplier(rutMultiplier === 1 ? 2 : 1)}
+            >
+              {rutMultiplier === 1 ? t("applyRut") : t("removeRut")}
+            </Button>
           </div>
         </div>
         <ul className="flex flex-col gap-5 md:whitespace-nowrap">
@@ -33,24 +40,44 @@ const Prices = () => {
           <li className="flex justify-between items-baseline gap-4 font-semibold text-[1.25rem]/[1.3125rem]">
             <p>{t("houseCleaning")}</p>
             <div className="w-full border border-white border-dashed" />
-            <p>{innitialPrice / rutMultiplier} SEK/{t("hour")}</p>
+            <p>
+              {rutMultiplier === 1 ? "450" : "360"} SEK/{t("hour")}
+            </p>
           </li>
           <li className="flex justify-between items-baseline gap-4 font-semibold text-[1.25rem]/[1.3125rem]">
             <p>{t("officeCleaning")}</p>
             <div className="w-full border border-white border-dashed" />
-            <p>{Math.ceil(innitialPrice * 1.333)} SEK/{t("hour")}</p>
+            <p>600 SEK/{t("hour")}</p>
           </li>
           <li className="flex justify-between items-baseline gap-4 font-semibold text-[1.25rem]/[1.3125rem]">
             <p>{t("deepCleaning")}</p>
             <div className="w-full border border-white border-dashed" />
-            <p>{Math.floor(innitialPrice / rutMultiplier * 1.067)} SEK/{t("hour")}</p>
+            <p>
+              {rutMultiplier === 1 ? "480" : "384"} SEK/{t("hour")}
+            </p>
           </li>
           <li className="flex justify-between items-baseline gap-4 font-semibold text-[1.25rem]/[1.3125rem]">
             <p>{t("moveInMoveOutCleaning")}</p>
             <div className="w-full border border-white border-dashed" />
-            <p>{Math.ceil(innitialPrice / rutMultiplier * 1.111)} SEK/{t("hour")}</p>
+            <p>
+              {rutMultiplier === 1 ? "500" : "400"} SEK/{t("hour")}
+            </p>
           </li>
         </ul>
+        <div>
+          <Button
+            className="text-white px-0"
+            variant="link"
+            onClick={() => setToggleInfo(!toggleInfo)}
+          >
+            {!toggleInfo ? t("learnMore") : t("closePanel")}
+          </Button>
+        </div>
+        {toggleInfo && (
+          <div>
+            <p>{t("RUTinfo")}</p>
+          </div>
+        )}
       </div>
     </section>
   );
